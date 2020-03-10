af7cfd913b620bff520b5176eafbc53dd21b15c5
Gunnar R <gunnarrosenberg@gmail.com>
2020-03-09 23:59:43 -0700

    feat(new views + refactoring): added settings + updated

---
+++ client/src/views/user/Billing.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ProjectSideNav from "./dashboards/components/ProjectSideNav";
+
+class Billing extends Component {
+    render() {
+        return (
+            <div>
+                <ProjectSideNav/>
+                <div className={'dash-content-container'}>
+
+                    Billing
+
+                </div>
+            </div>
+        )
+    }
+}
